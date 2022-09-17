c9bea596fe30071dd2a85deab1b04626f29d6b34
poetra-d <dewanggapramanaputra@gmail.com>
2022-09-17 19:33:29 +0700

    janji ga marah promise async/await

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+try {
+    // gunakan kata await untuk mendapatkan "data" hasil resolve-nya
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+    let counter = 0;
+    return new Promise((resolve) => {
+        counter += theaterIXX.filter((jumlah) => jumlah.hasil === emosi).length;
+        counter += theaterVGC.filter((jumlah) => jumlah.hasil === emosi).length;
+        resolve(counter);
+    });
+  } catch (errorJumlah) {
+    console.log(errorJumlah);
+  }
+};
 
 module.exports = {
   promiseOutput,
